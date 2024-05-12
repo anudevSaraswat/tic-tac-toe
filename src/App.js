7efaf795f6f876bb7f917251657c412c4b67b563
anudevSaraswat <mwanudev@gmail.com>
2024-05-13 02:07:09 +0530

    wip: css for cell-x and cell-o and dynamic className

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { TicTacToeGrid } from './TicTacToeGrid';
 function App() {
   return (
     <div className="App">
+      <h1>Tic Tac Toe</h1>
       <TicTacToeGrid></TicTacToeGrid>
     </div>
   );
--- src/TicTacToeGrid.js
+++ src/TicTacToeGrid.js
@@ -26,22 +26,22 @@ export function TicTacToeGrid() {
         <div>
             <table className="table">
                 <tr className="table-row-1">
-                    <td className="cell" onClick={() => handleClick(0)}>{cells[0]}</td>
-                    <td className="cell" onClick={() => handleClick(1)}>{cells[1]}</td>
-                    <td className="cell" onClick={() => handleClick(2)}>{cells[2]}</td>
+                    <td className={cells[0] ? `cell-${cells[0]}`: 'cell' } onClick={() => handleClick(0)}>{cells[0]}</td>
+                    <td className={cells[1] ? `cell-${cells[1]}`: 'cell'} onClick={() => handleClick(1)}>{cells[1]}</td>
+                    <td className={cells[2] ? `cell-${cells[2]}`: 'cell'} onClick={() => handleClick(2)}>{cells[2]}</td>
                 </tr>
                 <tr className="table-row-2">
-                    <td className="cell" onClick={() => handleClick(3)}>{cells[3]}</td>
-                    <td className="cell" onClick={() => handleClick(4)}>{cells[4]}</td>
-                    <td className="cell" onClick={() => handleClick(5)}>{cells[5]}</td>
+                    <td className={cells[3] ? `cell-${cells[3]}`: 'cell'} onClick={() => handleClick(3)}>{cells[3]}</td>
+                    <td className={cells[4] ? `cell-${cells[4]}`: 'cell'} onClick={() => handleClick(4)}>{cells[4]}</td>
+                    <td className={cells[5] ? `cell-${cells[5]}`: 'cell'} onClick={() => handleClick(5)}>{cells[5]}</td>
                 </tr>
                 <tr className="table-row-3">
-                    <td className="cell" onClick={() => handleClick(6)}>{cells[6]}</td>
-                    <td className="cell" onClick={() => handleClick(7)}>{cells[7]}</td>
-                    <td className="cell" onClick={() => handleClick(8)}>{cells[8]}</td>
+                    <td className={cells[6] ? `cell-${cells[6]}`: 'cell'} onClick={() => handleClick(6)}>{cells[6]}</td>
+                    <td className={cells[7] ? `cell-${cells[7]}`: 'cell'} onClick={() => handleClick(7)}>{cells[7]}</td>
+                    <td className={cells[8] ? `cell-${cells[8]}`: 'cell'} onClick={() => handleClick(8)}>{cells[8]}</td>
                 </tr>
             </table>
-            <button onClick={handleButtonClick}>Reset</button>
+            <button className="reset" onClick={handleButtonClick}>Reset</button>
         </div>
 
     )
